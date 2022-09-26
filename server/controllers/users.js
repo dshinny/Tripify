287a0565eed5f6fd186f3d5a27e24fe2355921ce
Daniel Shin <daniel990402@gmail.com>
2022-09-26 13:00:38 -0400

    implemented user signup and login

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -2,11 +2,35 @@ const User = require('../models/').User;
 const bcrypt = require('bcrypt');
 
 const login = async (req, res) => {
-
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  const validPassword = await bcrypt.compare(password, user.password)
+  if (validPassword) {
+    res.send(user)
+  } else {
+    res.status(404).end('Incorrect email or password');
+  }
 }
 
 const signup = async (req, res) => {
-
+  const { first, last, email, password } = req.body;
+  const userExists = await User.find({ email });
+  if (password.length === 0) {
+    res.status(404).end('Password required')
+  }
+  if (userExists.length === 0) {
+    const hash = await bcrypt.hash(password, 10);
+    const user = new User({
+      first,
+      last,
+      email,
+      password: hash
+    })
+    await user.save();
+    res.send(user)
+  } else {
+    res.status(404).end('Email already registered')
+  }
 }
 
 module.exports = {
