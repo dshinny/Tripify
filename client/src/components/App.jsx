39b61de57f8ca902b73ebfc4dd5dffff938e3569
Daniel Shin <daniel990402@gmail.com>
2022-09-26 13:11:59 -0400

    updated main app

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,19 +1,33 @@
 import React, { useState, useEffect } from 'react';
+
+import { getPlacesData, getWeatherData } from '../api/index.js';
 import Login from './Login.jsx';
 import Signup from './Signup.jsx';
+import Header from './Header.jsx';
 
 import { Divider } from '@mui/material';
 import styled from 'styled-components'
 
 const App = () => {
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(true);
   const [user, setUser] = useState(null);
+
+  const signOut = () => {
+    const conf = confirm('Sign Out?')
+    if (conf) {
+      setLoggedIn(false);
+    }
+  }
+
   return (
-    <Flex>
-      <Signup setLoggedIn={setLoggedIn}/>
-      <Divider orientation="vertical" flexItem />
-      <Login setLoggedIn={setLoggedIn}/>
-    </Flex>
+    <>
+      <Header loggedIn={loggedIn} signOut={signOut}/>
+      <Flex>
+        <Signup setLoggedIn={setLoggedIn}/>
+        <Divider orientation="vertical" flexItem />
+        <Login setLoggedIn={setLoggedIn}/>
+      </Flex>
+    </>
   )
 }
 
