fc189f415bb515682cb6673b4a7540c01ef3a102
Daniel Shin <daniel990402@gmail.com>
2022-09-26 12:43:14 -0400

    updated user states

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Login from './Login.jsx';
+import Signup from './Signup.jsx';
+
+import { Divider } from '@mui/material';
+import styled from 'styled-components'
 
 const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
   return (
-    <h1>Hello World!</h1>
+    <Flex>
+      <Signup setLoggedIn={setLoggedIn}/>
+      <Divider orientation="vertical" flexItem />
+      <Login setLoggedIn={setLoggedIn}/>
+    </Flex>
   )
 }
+
+const Flex = styled.div`
+  display: flex;
+  margin-top: 8%;
+`
 
 export default App;
