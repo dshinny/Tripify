56478bcb08cf402fa644d31f86d2f6a5e1fd6528
Daniel Shin <daniel990402@gmail.com>
2022-09-28 16:03:27 -0400

    updated header styling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { Autocomplete } from '@react-google-maps/api';
 
-import styled from 'styled-components';
-import { AppBar, Paper, InputBase, Divider, IconButton, Typography, Avatar, Stack, Button } from '@mui/material';
+import { AppBar, Box, Toolbar, IconButton, Typography, InputBase, Badge, MenuItem, Menu } from '@mui/material';
+import { styled, alpha } from '@mui/material/styles';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 
-const Header = ({ setCoordinates, setOpenCol, loggedIn, signOut }) => {
+const Header = ({ setCoordinates, setOpenCol, loggedIn, signOut, user }) => {
 
   const [autocomplete, setAutocomplete] = useState(null);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
 
   const onLoad = (autoC) => {
     setAutocomplete(autoC);
@@ -20,46 +24,164 @@ const Header = ({ setCoordinates, setOpenCol, loggedIn, signOut }) => {
     setCoordinates({ lat, lng });
   }
 
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  }
+
+  const handleLogout = () => {
+    setAnchorEl(null);
+    signOut();
+  }
+
+  const handleItineraryClick = () => {
+    setAnchorEl(null);
+    setOpenCol(true)
+  }
+
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id='primary-search-account-menu'
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMenuOpen}
+      onClose={() => setAnchorEl(null)}
+    >
+      <MenuItem onClick={handleLogout}>Logout</MenuItem>
+      <MenuItem onClick={handleItineraryClick}>Itinerary</MenuItem>
+    </Menu>
+  );
+
   return (
-    <AppBar position='static' color='inherit' sx={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'row'}}>
-      <Typography variant='h5' sx={{ padding: '10px 10px' }}>
-        Tripify
-      </Typography>
-      {loggedIn ? (
-        <Flex>
-          <Paper
-            component="form"
-            sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 230, marginRight: 2 }}
-          >
-            <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
-            <InputBase
-              sx={{ ml: 1, flex: 1 }}
-              placeholder="Search"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-            </Autocomplete>
-            <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-            <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-              <SearchIcon />
-            </IconButton>
-          </Paper>
-          <IconButton sx={{ p: '10px' }} aria-label="menu" onClick={() => setOpenCol(true)}>
-            <MenuIcon />
-          </IconButton>
-          <Button variant='contained' onClick={signOut}>Logout</Button>
-        </Flex>
-      ) : (
-        <></>
-      )}
-    </AppBar>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          {loggedIn ? (
+            <>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ display: { xs: 'none', sm: 'block' } }}
+              >
+                Tripify
+              </Typography>
+              <Search>
+                  <SearchIconWrapper>
+                    <SearchIcon />
+                  </SearchIconWrapper>
+                <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+                <StyledInputBase
+                  placeholder="Search…"
+                  inputProps={{ 'aria-label': 'search' }}
+                  />
+                  </Autocomplete>
+              </Search>
+              <Box sx={{ flexGrow: 1 }} />
+              <Typography
+                variant="subtitle2"
+                noWrap
+                component="div"
+                sx={{ display: { xs: 'none', sm: 'block' } }}
+              >
+                {`${user.first} ${user.last}`}
+              </Typography>
+              <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                <IconButton
+                  size="large"
+                  edge="end"
+                  aria-label="account of current user"
+                  aria-controls='primary-search-account-menu'
+                  aria-haspopup="true"
+                  onClick={handleProfileMenuOpen}
+                  color="inherit"
+                >
+                  <AccountCircle />
+                </IconButton>
+              </Box>
+            </>
+          ) : (
+            <>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ display: { xs: 'none', sm: 'block' } }}
+              >
+                Tripify
+              </Typography>
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
+      {renderMenu}
+    </Box>
   );
 }
 
-const Flex = styled.div`
-  display: flex;
-  flex-direction: row;
-  padding-right: 1%;
-  align-items: center;
-`
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
 
 export default Header;
