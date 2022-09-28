40a1899043c7cad18ac79af279294aa1173270e6
Daniel Shin <daniel990402@gmail.com>
2022-09-28 16:06:13 -0400

    added styling changes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -15,7 +15,7 @@ import styled from 'styled-components'
 
 const App = () => {
 
-  const [loggedIn, setLoggedIn] = useState(true);
+  const [loggedIn, setLoggedIn] = useState(false);
   const [user, setUser] = useState(null);
 
   const [places, setPlaces] = useState([]);
